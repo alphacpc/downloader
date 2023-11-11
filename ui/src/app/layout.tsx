877df2e9ed82c26a42af3ad20edeef41a25baec2
alphacpc <alphacpc@gmail.com>
2023-11-11 18:29:32 +0000

    design navbar

--- ui/src/app/layout.tsx
+++ ui/src/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
+import '../styles/globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+
+// const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -15,8 +18,14 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="fr">
+      <body>
+      <Navbar/>
+        <main className="relative overflow-hidden">
+          {children}
+        </main>
+        <Footer/>
+      </body>
     </html>
   )
 }
---
+++ ui/src/components/Navbar.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image"
+import Link from "next/link"
+import { NAV_LINKS } from '@/constants';
+import Button from "./Button";
+
+
+const Navbar = () => {
+  return (
+    <nav className="flex justify-around max-container padding-container relative z-30 py-5">
+        <Link href="/">
+            {/* <Image src="../assets/images/DakarScript.png" alt="logo" width={74} height={29 }/> */}
+            <span className="flex justify-center items-center font-bold text-3xl capitalize font-sans ">dieul<span className="text-orange-600 uppercase">ko</span></span>
+        </Link>
+
+        <ul className="hidden h-full gap-12 lg:flex">
+            {NAV_LINKS.map((link)=>(
+                <Link href={link.href} key={link.key}
+                        className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
+                >
+                    {link.label}
+                </Link>
+            ))} 
+        </ul>
+
+        <div className="lg:flexCenter hidden">
+            <Button type="button" title="Login" 
+            icon="/user.svg" variant="btn_dark_green"/>
+        </div>
+
+    </nav>
+  )
+}
+
+export default Navbar

696bc6e7a63c56ebaeac9181931aded59cf8fdd0
alphacpc <alphacpc@gmail.com>
2024-05-11 18:38:26 +0000

    design page modop

--- ui/src/app/modop/page.tsx
+++ ui/src/app/modop/page.tsx
@@ -1,43 +1,49 @@
 export default function Page() {
-        return <div>
 
-Manuel d'utilisation de l'application de téléchargement de vidéos YouTube
-Introduction
-Bienvenue dans l'application de téléchargement de vidéos YouTube ! Cette application vous permet de télécharger facilement des vidéos à partir de liens YouTube, vous permettant ainsi de les regarder hors ligne, de les sauvegarder localement ou de les partager avec vos amis.
+        const h1 = "text-4xl text-center font-bold py-10"
+        const h2 = "text-xl font-bold my-5 uppercase mt-20 text-green-600"
+        const ul = "ul-list"
 
-Table des matières
-Installation
-Utilisation de base
-Fonctionnalités avancées
-Problèmes courants et solutions
-Support technique
-1. Installation
-Pour installer l'application de téléchargement de vidéos YouTube, suivez ces étapes simples :
+        return <div className=" min-h-[80vh] text-lg mx-[15%]">
 
-Téléchargez le fichier d'installation à partir du site Web officiel de l'application.
-Exécutez le fichier d'installation et suivez les instructions à l'écran pour installer l'application sur votre appareil.
-Une fois l'installation terminée, lancez l'application en cliquant sur l'icône correspondante dans le menu de votre système d'exploitation.
-2. Utilisation de base
-L'utilisation de l'application est simple et intuitive :
+                <h1 className={h1}>Manuel d'utilisation de l'application de téléchargement de vidéos YouTube</h1>
 
-Copiez le lien de la vidéo YouTube que vous souhaitez télécharger.
-Ouvrez l'application de téléchargement de vidéos YouTube.
-Collez le lien dans la zone prévue à cet effet et appuyez sur le bouton de téléchargement.
-Attendez que le téléchargement soit terminé. Une fois terminé, la vidéo sera disponible dans votre dossier de téléchargement par défaut.
-3. Fonctionnalités avancées
-L'application propose également des fonctionnalités avancées pour améliorer votre expérience de téléchargement :
+                <div>
+                        <h2 className={h2}>Introduction</h2>
+                        <p>Bienvenue dans l'application de téléchargement de vidéos YouTube !
+                        Cette application vous permet de télécharger facilement des vidéos à partir 
+                        de liens YouTube, vous permettant ainsi de les regarder hors ligne, de les 
+                        sauvegarder localement ou de les partager avec vos amis.</p>
+                </div>
 
-Personnalisation des paramètres de téléchargement : Vous pouvez spécifier la qualité et le format de la vidéo téléchargée.
-Gestion de la file d'attente : Vous pouvez ajouter plusieurs liens à télécharger en même temps et gérer leur progression dans la file d'attente.
-Intégration avec d'autres services : L'application peut être intégrée à d'autres services de stockage cloud pour sauvegarder automatiquement vos vidéos téléchargées.
-4. Problèmes courants et solutions
-Si vous rencontrez des problèmes lors de l'utilisation de l'application, voici quelques solutions possibles :
+                <div>
+                        <h2 className={h2}>Utilisation de base</h2>
+                        <p>L'utilisation de l'application est simple et intuitive : </p>
+                        <ul className={ul}>
+                                <li>Copiez le lien de la vidéo YouTube que vous souhaitez télécharger.</li>
+                                <li>Ouvrez l'application de téléchargement de vidéos YouTube.</li>
+                                <li>Collez le lien dans la zone prévue à cet effet et appuyez sur le bouton de téléchargement.</li>
+                                <li>Attendez que le téléchargement soit terminé. Une fois terminé, la vidéo sera disponible dans votre dossier de téléchargement par défaut.</li>
+                        </ul>
+                </div>
+                
+                <div>
+                        <h2 className={h2}>Problèmes courants et solutions</h2>
+                        <p>Si vous rencontrez des problèmes lors de l'utilisation de l'application, voici quelques solutions possibles :</p>
+                        <ul className={ul}>
+                                <li>Problème : La vidéo ne se télécharge pas.</li>
+                                <p>Solution : Vérifiez votre connexion Internet et assurez-vous que le lien de la vidéo est valide.</p>
+                                <li>Problème : L'application se bloque ou ne répond pas.</li>
+                                <p>Solution : Essayez de redémarrer l'application ou de réinstaller l'application pour 
+                                résoudre les problèmes de performance.</p>
+                        </ul>
+                </div>
 
-Problème : La vidéo ne se télécharge pas.
-Solution : Vérifiez votre connexion Internet et assurez-vous que le lien de la vidéo est valide.
-Problème : L'application se bloque ou ne répond pas.
-Solution : Essayez de redémarrer l'application ou de réinstaller l'application pour résoudre les problèmes de performance.
-5. Support technique
-Si vous avez besoin d'aide supplémentaire ou si vous rencontrez des problèmes qui ne sont pas résolus dans ce manuel, n'hésitez pas à contacter notre équipe de support technique. Vous pouvez trouver nos coordonnées sur notre site Web officiel.
+                <div>
+                        <h2 className={h2}>Support technique</h2>
+                        <p>Si vous avez besoin d'aide supplémentaire ou si vous rencontrez des problèmes qui ne 
+                        sont pas résolus dans ce manuel, n'hésitez pas à contacter notre équipe de support technique. 
+                        Vous pouvez trouver nos coordonnées sur notre site Web officiel. </p>
+                </div>
         </div>
 }

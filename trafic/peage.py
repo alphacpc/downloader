36d5fbcadcf44612b1646d4b13765b9cb2866763
alphacpc <alphacpc@gmail.com>
2024-05-19 19:03:00 +0000

    updated content peage.py by added categ

---
+++ trafic/peage.py
@@ -0,0 +1,17 @@
+import random
+from time import sleep
+
+CAR_TYPE = ["moto", "vehicule", "minicar", "bus", "camion"]
+PEAGE_TAG = ["entrant", "sortant"]
+PEAGE_CHECKPOINT = ["pikine", "poste thiaroye", "ka"]
+
+while True:
+        print(random.choice(CAR_TYPE))
+        sleep(1)
+        
+        
+Catégorie de Véhicule	        Tronçon Diamniadio - AIBD
+C1, Moto	                500	1 000   400
+C2, Véhicule particulier	800	1 600   500
+C3, Camionnette	                1 200	2 400   800
+C4,C5 Car et Poids lourd	1 500	3 100   1000

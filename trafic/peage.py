c28d4a048207d145c8e45e56ad3c8e2869557ecd
alphacpc <alphacpc@gmail.com>
2024-05-19 19:12:34 +0000

    started v2 print

--- trafic/peage.py
+++ trafic/peage.py
@@ -1,12 +1,22 @@
 import random
 from time import sleep
 
-CAR_TYPE = ["moto", "vehicule", "minicar", "bus", "camion"]
 PEAGE_TAG = ["entrant", "sortant"]
-PEAGE_CHECKPOINT = ["pikine", "poste thiaroye", "ka"]
+PEAGE_CHECKPOINT = ["pikine", "poste thiaroye", "cap biche"]
+PEAGE_CATEG = [
+        {"categ": "C1", "label": "Moto", "price": 400},
+        {"categ": "C2", "label": "Vehicule particulier", "price": 500},
+        {"categ": "C3", "label": "Camionnette", "price": 800},
+        {"categ": "C4", "label": "Car", "price": 1000},
+        {"categ": "C5", "label": "Poids lourd", "price": 1000},
+]
+
+# categ - label - price - tag - checkpoint - datetime - date - time - datetime_categ_label_price_tag
 
 while True:
-        print(random.choice(CAR_TYPE))
+        print(random.choice(PEAGE_CATEG))
+        print(random.choice(PEAGE_TAG))
+        print(random.choice(PEAGE_CHECKPOINT))
         sleep(1)
         
         
